389a91cb23f5d3534abab9230727818ccf9f2a88
Sasha Mikheyev <sasha@homologo.us>
2013-09-15 11:44:13 +0900

    sfs updates

--- angsd.sh
+++ angsd.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#$ -q long
+#$ -q genomics
 #$ -j y
 #$ -cwd
 #$ -N angsd
@@ -11,18 +11,18 @@
 export TEMPDIR=/genefs/MikheyevU/temp
 export TEMP=/genefs/MikheyevU/temp
 export TMP=/genefs/MikheyevU/temp
-MAXMEM=3
 
 ref=../ref/amel4.5.fa
 
 
 #covert vcf file to bim file
-cat data/recalibrated.filtered.passed.vcf | awk -v OFS="\t" '$1!~/^#/ && $5!~/,/ {print $1,".",".",$2,$4,$5}' | uniq > data/angsd/snps.bim 
+cat data/recalibrated.filtered.passed.repeatmasked.vcf | awk -v OFS="\t" '$1!~/^#/ && $5!~/,/ {print $1,".",".",$2,$4,$5}' | uniq > data/angsd/snps.bim 
 
 #Conduct likelihood ratio test and call SNPs
-/apps/MikheyevU/popgen/angsd/angsd -filter data/angsd/snps.bim -ref $ref -doAsso 1 -yBin data/angsd/cases.txt -bam data/angsd/both.txt -out data/angsd/association.txt -doMaf 2 -GL 4 -doCounts 1 -doMajorMinor 3 -minLRT 24 -doSNP 1 -nThreads 6
+/apps/MikheyevU/popgen/angsd/angsd -filter data/angsd/snps.bim -ref $ref -doAsso 1 -yBin data/angsd/cases.txt -bam data/angsd/both.txt -out data/angsd/association.txt -doMaf 2 -GL 4 -doCounts 1 -doMajorMinor 4 -minLRT 24 -doSNP 1 -nThreads 12
 
-#Calculate allele frequencies for modern and old bees
-/apps/MikheyevU/popgen/angsd/angsd -filter data/angsd/snps.bim -ref $ref -bam data/angsd/old.txt -out data/angsd/old -doMaf 2 -GL 4  -doMajorMinor 4  -doCounts 1 -nThreads 6
+# Calculate allele frequencies, genotype frequencies for modern and old bees, using reference allele as major
 
-/apps/MikheyevU/popgen/angsd/angsd -filter data/angsd/snps.bim -ref $ref -bam data/angsd/modern.txt -out data/angsd/modern -doMaf 2 -GL 4  -doMajorMinor 4  -doCounts 1 -nThreads 6
+/apps/MikheyevU/popgen/angsd/angsd -filter data/angsd/snps.bim -ref $ref -bam data/angsd/old.txt -out data/angsd/old -doGeno 32 -doPost 1 -doMaf 2 -doMajorMinor 4 -GL 4 -doCounts 1 -nThreads 12
+
+/apps/MikheyevU/popgen/angsd/angsd -filter data/angsd/snps.bim -ref $ref -bam data/angsd/modern.txt -out data/angsd/modern -doGeno 32 -doPost 1 -doMaf 2 -doMajorMinor 4 -GL 4 -doCounts 1 -nThreads 12
--- sfs.sh
+++ sfs.sh
@@ -15,5 +15,10 @@ export TMP=/genefs/MikheyevU/temp
 a=(old modern)
 base=${a[$SGE_TASK_ID-1]}
 
-/apps/MikheyevU/popgen/angsd/angsd -anc ../ref/amel4.5.fa -nThreads 12 -bam data/angsd/old.txt -GL 1 -doCounts 1 -out data/angsd/$base -realSFS 1 
+#/apps/MikheyevU/popgen/angsd/angsd -anc ../ref/amel4.5.fa -nThreads 12 -bam data/angsd/old.txt -GL 1 -doCounts 1 -out data/angsd/$base -realSFS 1 
 
+/apps/MikheyevU/popgen/angsd/angsd0.538/misc/optimSFS -binput data/angsd/$base.sfs -nChr 64 -nThreads 12
+
+mv $base.sfs.ml data/angsd/
+
+/apps/MikheyevU/popgen/angsd/angsd0.538/misc/sfstools -sfsFile data/angsd/$base.sfs -nChr 64 -priorFile data/angsd/$base.sfs.ml -nThreads 12 -dumpBinary 1 > data/angsd/pops.sfs.norm
